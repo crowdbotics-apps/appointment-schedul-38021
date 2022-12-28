16ce55cb58a844e3eee466a8f530495e13e8a8ef
Crowdbotics <team@crowdbotics.com>
2022-12-28 18:22:29 +0000

    Update admin title

--- backend/scheduling_app_temp_38021/urls.py
+++ backend/scheduling_app_temp_38021/urls.py
@@ -35,9 +35,9 @@ urlpatterns = [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
 
-admin.site.site_header = "Scheduling app - Template"
-admin.site.site_title = "Scheduling app - Template Admin Portal"
-admin.site.index_title = "Scheduling app - Template Admin"
+admin.site.site_header = "Appointment Scheduler"
+admin.site.site_title = "Appointment Scheduler Admin Portal"
+admin.site.index_title = "Appointment Scheduler Admin"
 
 # swagger
 api_info = openapi.Info(
